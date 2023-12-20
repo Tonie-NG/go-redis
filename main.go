ce4f553820c39bba438cacff28634cb76e6e5d63
Tonie Victor <victornwanochi@gmail.com>
2023-12-20 10:56:53 +0100

    feat: implemented a basic handler for ping commands

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -26,14 +27,36 @@ func main() {
   for {
     resp := NewResp(conn)
 
-    _, err := resp.Read()
+    value, err := resp.Read()
 
     if err != nil {
       fmt.Println(err)
       return
     }
 
+    if value.typ != "array" {
+      fmt.Println("Invalid request, expected array")
+      continue
+    }
+
+    if len(value.array) == 0 {
+      fmt.Println("Invalid request, expected array length > 0")
+      continue
+    }
+
+    command := strings.ToUpper(value.array[0].bulk)
+    args := value.array[1:]
+
     writer := NewWriter(conn)
-    writer.Write(Value{typ: "string", str: "PONG"})
+    handler, ok := Handlers[command]
+
+    if !ok {
+      fmt.Println("Invalid command: ", command)
+      writer.Write(Value{typ: "string", str: ""})
+      continue
+    }
+
+    result := handler(args)
+    writer.Write(result)
   }
 }
